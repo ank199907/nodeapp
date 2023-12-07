9a0fff9e76206c4659743794386599114b322470
ank199907 <145426297+ank199907@users.noreply.github.com>
2023-12-07 17:33:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
+app.get('/', (req, res) => {
+    res.send('docker');
+});
 
 app.listen(port, () => {
     console.log(`Server is listening at http://18.234.88.214:${port}`);
